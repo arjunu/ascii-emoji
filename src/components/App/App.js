72e41f87297fac2faf5018b4bff12ef79e0bfdaf
Arjun <ar5junu@gmail.com>
2018-03-31 23:00:38 +0200

    Added couple of emoji to display

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { ToastContainer, toast } from 'react-toastify';
+import emoji from '../../emoji';
+
+const notify = () => {
+  toast.info("Copied to clipboard", {
+    position: toast.POSITION.BOTTOM_CENTER
+  });
+};
 
 export default class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="appWrapper">
+        <span className="title">ASCII EMOJI</span>
+
+        <ul className="list">
+          {emoji.map(item => (
+            <li key={item.name} className="list-item">
+              <CopyToClipboard text={item.value} onCopy={notify}>
+                <span>{item.value}</span>
+              </CopyToClipboard>
+            </li>
+          ))}
+        </ul>
+
+        <ToastContainer autoClose={2000} hideProgressBar/>
+      </div>
+    );
   }
 }
---
+++ src/emoji.js
@@ -0,0 +1,12 @@
+const emoji = [
+  {
+    name: "shrug",
+    value: "¯\\_(ツ)_/¯"
+  },
+  {
+    name: "angry",
+    value: "ಠ_ಠ"
+  }
+];
+
+export default emoji;
