71c3cfe762ce8a640a3ef17f87751e1a57624ae6
Arjun <ar5junu@gmail.com>
2018-04-01 19:12:01 +0200

    Added more emoji

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { ToastContainer, toast } from 'react-toastify';
 import emoji from '../../emoji';
 
 const notify = () => {
-  toast.info("Copied to clipboard", {
+  toast.info('Copied to clipboard', {
     position: toast.POSITION.BOTTOM_CENTER
   });
 };
@@ -13,19 +13,32 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="appWrapper">
-        <span className="title">ASCII EMOJI</span>
+        <header>
+          <span className="title">ASCII EMOJI</span>
+        </header>
 
         <ul className="list">
           {emoji.map(item => (
             <li key={item.name} className="list-item">
               <CopyToClipboard text={item.value} onCopy={notify}>
-                <span>{item.value}</span>
+                {Array.isArray(item.value) ? (
+                  <span>
+                    {item.value.map((line, index) => (
+                      <span key={index}>
+                        {line}
+                        <br />
+                      </span>
+                    ))}
+                  </span>
+                ) : (
+                  <span>{item.value}</span>
+                )}
               </CopyToClipboard>
             </li>
           ))}
         </ul>
 
-        <ToastContainer autoClose={2000} hideProgressBar/>
+        <ToastContainer autoClose={2000} hideProgressBar />
       </div>
     );
   }
--- src/emoji.js
+++ src/emoji.js
@@ -1,11 +1,86 @@
 const emoji = [
   {
-    name: "shrug",
-    value: "¯\\_(ツ)_/¯"
+    name: 'shrug',
+    value: '¯\\_(ツ)_/¯',
+    tags: ['shrug', 'dunno']
   },
   {
-    name: "angry",
-    value: "ಠ_ಠ"
+    name: 'angry',
+    value: 'ಠ_ಠ'
+  },
+  {
+    name: 'angry2',
+    value: '(╯°□°）╯︵ ┻━┻',
+    tags: ['angry', 'table', 'throw', 'table flip']
+  },
+  {
+    name: 'angry3',
+    value: '┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻',
+    tags: ['angry', 'table', 'table flip']
+  },
+  {
+    name: 'person_flip',
+    value: '（╯°□°）╯︵(\\ .o.)\\',
+    tags: ['angry', 'person flip']
+  },
+  {
+    name: 'table',
+    value: '┬──┬ ノ( ゜-゜ノ)',
+    tags: ['table']
+  },
+  {
+    name: 'strength',
+    value: "(ง'̀-'́)ง"
+  },
+  {
+    name: 'strength2',
+    value: 'ᕙ(⇀‸↼‶)ᕗ',
+    tags: ['strength']
+  },
+  {
+    name: 'yeeeaaahhh',
+    value: ['( •_•)', '( •_•)>⌐■-■', '(⌐■_■)'],
+    tags: ['yeeeaaahhh', 'cool', 'glasses']
+  },
+  {
+    name: 'demon',
+    value: '(屮ﾟДﾟ)屮 Y U NO',
+    tags: ['demon']
+  },
+  {
+    name: 'floating_table',
+    value: '(/¯◡ ‿ ◡)/¯ ~ ┻━┻',
+    tags: ['jedi', 'floating table', 'telekinetic']
+  },
+  {
+    name: 'you',
+    value: '(☞ﾟヮﾟ)☞',
+    tags: ['you']
+  },
+  {
+    name: 'gasp',
+    value: 'L(° O °L)',
+    tags: ['gasp']
+  },
+  {
+    name: 'kiss',
+    value: '(づ￣ ³￣)づ',
+    tags: ['kiss']
+  },
+  {
+    name: 'wink',
+    value: '◕‿↼',
+    tags: ['wink']
+  },
+  {
+    name: 'delicious',
+    value: '(っˆڡˆς)',
+    tags: ['yummy', 'delicious', 'tasty']
+  },
+  {
+    name: 'hey',
+    value: "(>'o')>",
+    tags: []
   }
 ];
 
